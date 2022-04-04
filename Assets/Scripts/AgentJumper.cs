099689632c9760151fa625b99d1196500671f816
StefSantens <stef.santens@gmail.com>
2022-04-04 15:58:38 +0200

    Minor fixes and Added Rewards

--- Assets/Scripts/AgentJumper.cs
+++ Assets/Scripts/AgentJumper.cs
@@ -6,16 +6,17 @@ using Unity.MLAgents.Actuators;
 
 public class AgentJumper : Agent
 {
-    Rigidbody rBody;
+    Rigidbody _rBody;
     public bool isGrounded = true;
     public float jumpHeigth = 10f;
 
     public override void Initialize()
     {
-        rBody = GetComponent<Rigidbody>();
+        _rBody = GetComponent<Rigidbody>();
     }
     public override void OnEpisodeBegin()
     {
+        SetReward(1.0f);
         isGrounded = true;
         transform.localPosition = new Vector3(7.5f, 0.5f, 0f);
     }
@@ -25,15 +26,18 @@ public class AgentJumper : Agent
         if (isGrounded && actionBuffers.DiscreteActions[0] == 1)
         {
             isGrounded = false;
-            rBody.velocity = new Vector3(0, jumpHeigth, 0);
+            _rBody.velocity = new Vector3(0, jumpHeigth, 0);
+            AddReward(-0.001f);
         }
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
     {
         var discreteActionsout = actionsOut.DiscreteActions;
-        if (Input.GetKey(KeyCode.Space) == true)
+        if(Input.GetAxis("Jump") == 1)
+        {
             discreteActionsout[0] = 1;
+        }
     }
 
     void OnTriggerEnter(Collider other)
@@ -42,6 +46,7 @@ public class AgentJumper : Agent
         {
             Debug.Log("Hit Obstacle");
             Destroy(other.gameObject);
+            SetReward(-1.0f);
             EndEpisode();
         }
     }
